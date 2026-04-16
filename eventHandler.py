b940c3bf70c2a5031e30be932a8f790d206085fa
Harsh Naidu <naiduharsh45@gmail.com>
2026-04-16 16:13:12 +0530

    Now the console also shows the number of commits :)

--- eventHandler.py
+++ eventHandler.py
@@ -11,22 +11,22 @@ def load_activity(username):
     else:
         print(f"Failed to retrieve activity for user {username}. Status code: {response.status_code}")
 
-# def count_commits(username, repo):
-#     url = f"https://api.github.com/repos/{username}/{repo}/commits"
-#     response = requests.get(url)
+def count_commits(username, repo):
+    url = f"https://api.github.com/repos/{username}/{repo}/commits"
+    response = requests.get(url)
 
-#     if response.status_code == 200:
-#         commits = response.json()
-#         return len(commits)
-#     else:
-#         print(f"Failed to retrieve commits for {username}/{repo}. Status code: {response.status_code}")
-#         return None
+    if response.status_code == 200:
+        commits = response.json()
+        return len(commits)
+    else:
+        print(f"Failed to retrieve commits. Status code: {response.status_code}")
+        return 0
 
 def display_activity(activity):
     for event in activity:
         if event['type'] == 'PushEvent':
             created_at = datetime.strptime(event['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-            print(f"Pushed to {event['repo']['name']} at {created_at.strftime('%Y-%m-%d %H:%M:%S')}\n--------------------------------------------------------------")
+            print(f"Pushed to {event['repo']['name']} at {created_at.strftime('%Y-%m-%d %H:%M:%S')} | Commits: {count_commits(event['repo']['name'].split('/')[0], event['repo']['name'].split('/')[1])}\n--------------------------------------------------------------")
         elif event['type'] == 'CreateEvent':
             created_at = datetime.strptime(event['created_at'], '%Y-%m-%dT%H:%M:%SZ')
             print(f"Created {event['payload']['ref_type']} {event['payload']['ref']} in {event['repo']['name']} at {created_at.strftime('%Y-%m-%d %H:%M:%S')}\n--------------------------------------------------------------")
